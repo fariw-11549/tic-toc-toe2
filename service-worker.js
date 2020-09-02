e6ef7055e29a84dea00dbc09934ddbe73caa5b51
fariw-11549 <chaiwat.si.61@ubu.ac.th>
2020-09-02 11:09:27 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fariw-11549/tic-toc-app/precache-manifest.030b07d87878cab8582e494818f818da.js"
+  "/fariw-11549/tic-toc-toe2/precache-manifest.b9fea7df97e61e48e2d5101651785e71.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fariw-11549/tic-toc-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fariw-11549/tic-toc-toe2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
